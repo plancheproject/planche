ffc6cf8f90b2054172187e353ec5095f59b6508a
Jeong Ju Won <jjwcom@nate.com>
2014-07-20 14:48:10 +0900

    Host 설정 파

--- config.js
+++ config.js
@@ -2,74 +2,20 @@ var Planche = Planche || {};
 Planche.config = {
     hosts : [
         {
-            host_name : 'Iruen', 
+            host_name : 'Local Example', 
             http_tunneling : 'http://192.168.0.20/tunnel.php',
             host    : 'localhost',
             user    : 'root',
-            pass    : 'iruen',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 1', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
+            pass    : 'password',
             charset : 'utf8',
             port    : 3306
         },
         {
             host_name : 'DB Server 2', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/planche_tnl.php',
+            http_tunneling : 'http://example.com/planche_tnl.php',
             host    : 'localhost',
-            user    : 'websql',
-            pass    : 'mvp2002',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 3', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 4', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 5', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 6', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
-            charset : 'utf8',
-            port    : 3306
-        },
-        {
-            host_name : 'DB Server 7', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/tunnel.php',
-            host    : 'localhost',
-            user    : 'Planche',
-            pass    : 'mvp2002',
+            user    : 'user',
+            pass    : 'password',
             charset : 'utf8',
             port    : 3306
         }
