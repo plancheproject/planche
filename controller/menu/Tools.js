11fcdce143e6a79ab4595652b41cd8a035004132
Jeong Ju Won <jjwcom@nate.com>
2014-07-21 18:08:13 +0900

    Tools

---
+++ controller/menu/Tools.js
@@ -0,0 +1,22 @@
+Ext.define('Planche.controller.menu.Tools', {
+    extend: 'Ext.app.Controller',
+    added : false,
+    add : function(topBtn){
+
+        topBtn.menu.add([{
+            text : '..',
+        ]);
+
+        this.added = true;
+    },
+
+    show : function(topBtn){
+
+        if(!this.added){
+
+            this.add(topBtn);
+        }
+
+        topBtn.menu.showBy(topBtn);
+    }
+});
