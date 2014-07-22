fd512cf414a08c4c649730ea822a77c91598e740
Jeong Ju Won <jjwcom@nate.com>
2014-07-23 00:05:12 +0900

    Edit 기능 보강

--- controller/menu/Edit.js
+++ controller/menu/Edit.js
@@ -4,35 +4,197 @@ Ext.define('Planche.controller.menu.Edit', {
     add : function(topBtn){
 
         topBtn.menu.add([{
-            text : 'Refresh Object Browser'
+            text : 'Refresh Object Browser',
+            scope : this.application,
+            allowDisable : function(topBtn, menu){
+
+                if(!this.getActiveMainTab()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Excute Query'
+            text : 'Excute Query',
+            scope : this.application,
+            allowDisable : function(topBtn, menu){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },
         '-'
         ,{
-            text : 'SQL Formatter'
+            text : 'SQL Formatter',
+            scope : this.application,
+            allowDisable : function(topBtn, menu){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },
         '-'
         ,{
-            text : 'Undo'
+            text : 'Undo',
+            scope : this.application,
+            handler : function(){
+
+                this.getActiveEditor().undo(); 
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                if(this.getActiveEditor().historySize().undo < 1){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Redo'
+            text : 'Redo',
+            scope : this.application,
+            handler : function(){
+
+                this.getActiveEditor().redo();                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                if(this.getActiveEditor().historySize().redo < 1){
+
+                    return true;
+                }
+
+                return false;
+            }
         },
         '-'
         ,{
-            text : 'Cut'
+            text : 'Cut',
+            scope : this.application,
+            handler : function(){
+
+                this.getActiveEditor().cut();                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                if(!this.getActiveEditor().somethingSelected()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Copy'
+            text : 'Copy',
+            scope : this.application,
+            handler : function(){
+
+                this.getActiveEditor().copy();                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                if(!this.getActiveEditor().somethingSelected()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Paste'
+            text : 'Paste',
+            scope : this.application,
+            handler : function(){
+
+                this.getActiveEditor().paste();                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },
         '-'
         ,{
-            text : 'Find'
+            text : 'Find',
+            scope : this.application,
+            handler : function(){
+
+                this.openWindow('command.Find');                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Find Next'
+            text : 'Find Next',
+            scope : this.application,
+            handler : function(){
+
+                this.openWindow('command.Find');                
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+
+                return false;
+            }
         },{
-            text : 'Replace'
+            text : 'Replace',
+            scope : this.application,
+            handler : function(){
+
+                this.openWindow('command.Replace');               
+            },
+            allowDisable : function(){
+
+                if(!this.getActiveEditor()){
+
+                    return true;
+                }
+                
+                return false;
+            }
         }]);
 
         this.added = true;
@@ -44,6 +206,22 @@ Ext.define('Planche.controller.menu.Edit', {
 
             this.add(topBtn);
         }
+
+        Ext.Array.each(topBtn.menu.query('menuitem'), function(menu, idx){
+            
+            switch(typeof menu.allowDisable){
+
+                case 'function':
+
+                    menu.setDisabled(menu.allowDisable.apply(menu.scope || menu, [topBtn, menu]));
+                    break;
+
+                case 'boolean' :
+
+                    menu.setDisabled(menu.allowDisable);
+                    break;
+            }
+        });
 
         topBtn.menu.showBy(topBtn);
     }
