1733e6d4597d22e5ad4cc8982c16788a45b098c5
Jeong Ju Won <jjwcom@nate.com>
2014-07-25 11:03:46 +0900

    config.js 수정

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ Planche.config = {
         },
         {
             host_name : 'DB Server 2', 
-            http_tunneling : 'http://extjs.makewebapp.net/webql/planche_tnl.php',
+            http_tunneling : 'http://example.com/planche_tnl.php',
             host    : 'localhost',
-            user    : 'websql',
-            pass    : 'mvp2002',
+            user    : 'user',
+            pass    : 'password',
             charset : 'utf8',
             port    : 3306
         }
