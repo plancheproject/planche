9d05ad6f5bab8b72d152fd11209e1707ca45756c
Jeong Ju Won <jjwcom@nate.com>
2014-08-06 15:33:16 +0900

    기능 수정, 추가
    새로고침 UI 추가
    창 타이틀 올바르게 변경

--- controller/command/Process.js
+++ controller/command/Process.js
@@ -1,22 +1,12 @@
 Ext.define('Planche.controller.command.Process', {
     extend: 'Ext.app.Controller',
-
-    initWindow : function(result){
-
-        var columns = this.makeListColumns();
-
-        var fields = [];
-        Ext.each(columns, function(obj){
-
-            fields.push(obj.dataIndex);
-        });
-
-        var records = result.records;
+    grid : null,
+    initWindow : function(){
 
         Ext.create('Planche.lib.Window', {
             id : 'window-'+this.id,
             stateful: true,
-            title : 'User Manager',
+            title : 'Show Process List',
             layout : 'fit',
             bodyStyle:"background-color:#FFFFFF",
             width : 900,
@@ -29,20 +19,7 @@ Ext.define('Planche.controller.command.Process', {
             shadow : false,
             autoShow : true,
             constrain : true,
-            items : [
-                Ext.create('Ext.grid.Panel', {
-                    id : 'connect-grid',
-                    border : false,
-                    columnLines: true,
-                    width : '100%',
-                    flex  : 1,
-                    columns : columns,
-                    store: Ext.create('Ext.data.Store', {
-                        fields:fields,
-                        data: records
-                    })
-                })
-            ],
+            items : this.initGrid(),
             buttons : [{
                 text : 'Kill Process',
                 scope : this,
@@ -56,22 +33,119 @@ Ext.define('Planche.controller.command.Process', {
                 handler : function(btn, e){
                     
                     var win = btn.up('window');
-                    win.hide();
+                    win.destroy();
                 }
-            }]
+            }],
+            listeners : {
+                scope : this,
+                boxready : function(){
+
+                    this.loadList();
+                }
+            }
         });
     },
 
+    initGrid : function(){
+
+        var columns = this.makeListColumns();
+
+        var fields = [];
+        Ext.each(columns, function(obj){
+
+            fields.push(obj.dataIndex);
+        });
+
+        this.grid = Ext.create('Ext.grid.Panel', {
+            border : false,
+            columnLines: true,
+            width : '100%',
+            flex  : 1,
+            columns : columns,
+            store: Ext.create('Ext.data.Store', {
+                fields: fields
+            }),
+            tbar : [
+                { xtype: 'text',  text : 'Refresh Per Sec', margin : '0 0 0 5'},
+                { xtype: 'textfield', value: 0, scope: this, width : 40, margin : '0 0 0 5', listeners : {
+                    scope : this,
+                    specialkey: function (field, el) {
+
+                        if (el.getKey() == Ext.EventObject.ENTER){
+
+                            this.loadList();
+                        }
+                    }
+                }},
+                { xtype: 'button', text: 'Refresh', cls : 'btn', scope: this, margin : '0 0 0 5', scope : this, handler : function(btn){
+
+                    this.loadList();
+                }},
+                { xtype: 'button', text: 'Stop', cls : 'btn', scope: this, margin : '0 0 0 5', scope : this, handler : function(btn){
+
+                    var textRefreshPerSec = this.grid.down('text[text=Refresh Per Sec]').next();
+
+                    textRefreshPerSec.setValue(0);
+                }}
+            ]
+        });
+
+        return this.grid;
+    },
+
+    loadList : function(){
+
+        var textRefreshPerSec = this.grid.down('text[text=Refresh Per Sec]').next();
+
+        var refreshPerSec = parseFloat(textRefreshPerSec.getValue());
+
+        var app = this.application;
+
+        var node = app.getSelectedNode();
+        var db = app.getParentNode(node);
+        app.tunneling({
+            db : db,
+            query : app.getEngine().getQuery('SHOW_PROCESS_LIST', db),
+            success : Ext.Function.bind(function(config, response){
+                
+                var records = this.makeRecords(response.fields, response.records);
+
+                this.grid.store.loadData(records);
+                this.grid.setLoading(false);
+
+                if(refreshPerSec > 0){
+
+                    setTimeout(Ext.Function.bind(this.loadList, this), refreshPerSec * 1000);
+                }
+            }, this)
+        });
+    },
+
+    makeRecords : function(fields, records){
+
+        var tmp = [];
+        Ext.Array.each(records, function(row, ridx){
+
+            var record = {};
+            Ext.Array.each(fields, function(col, cidx){
+
+                record[col.name] = row[cidx];
+            });
+            tmp.push(record);
+        });
+
+        return tmp;
+    },
     makeListColumns : function(){   
         
         return [
             { text: 'Id', dataIndex: 'Id', width : 100},
             { text: 'User', dataIndex: 'User', width : 100},
-            { text : 'Host', xtype :'checkcolumn', dataIndex : 'Host', width : 60 },
-            { text : 'Db', xtype :'checkcolumn', dataIndex : 'Db', width : 60 },
-            { text : 'Command', xtype :'checkcolumn', dataIndex : 'Command', width : 60 },
-            { text : 'State', xtype :'checkcolumn', dataIndex : 'State', width : 60 },
-            { text : 'Info', xtype :'checkcolumn', dataIndex : 'Info', width : 60 }
+            { text : 'Host', dataIndex : 'Host', width : 60 },
+            { text : 'Db', dataIndex : 'Db', width : 60 },
+            { text : 'Command', dataIndex : 'Command', width : 60 },
+            { text : 'State', dataIndex : 'State', width : 60 },
+            { text : 'Info', dataIndex : 'Info', flex : 1 }
         ];
     }
 });
