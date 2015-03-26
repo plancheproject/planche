fd35461123a65df5d601a9cfef513a9968d7bc24
Jeong Ju Won <jjwcom@nate.com>
2015-03-26 16:16:26 +0900

    Add yuidocjs and build documentation

---
+++ build/apidocs/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Planche.view.Main",
+        "Planche.view.Viewport"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
