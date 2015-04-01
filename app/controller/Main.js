791a352c89ed2ac19aa4433df3b27f0d2a6fdb2b
Jeong Ju Won <jjwcom@nate.com>
2015-04-01 18:17:38 +0900

    remove controllers attr

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -1,10 +1,5 @@
 Ext.define('Planche.controller.Main', {
     extend: 'Ext.app.Controller',
-    controllers : [
-    	'Planche.controller.layout.Toolbar',
-    	'Planche.controller.layout.ConnectTab'
-    	// 'Planche.controller.layout.ConnectTab'
-    ],
     init : function(){
 
 
