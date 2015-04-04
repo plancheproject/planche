bb7efa30bacabb9431ca67e1c55f7c079b9c103d
Jeong Ju Won <jjwcom@nate.com>
2015-04-04 11:07:42 +0900

    add table info tab in edit scheme window

---
+++ app/controller/table/TableInfoTab.js
@@ -0,0 +1,13 @@
+Ext.define('Planche.controller.table.TableInfoTab', {
+    extend: 'Ext.app.Controller',
+    views : [
+        'table.TableInfoTab'
+    ],
+    init : function(){
+        
+        this.control({
+            'table-info-tab' : {
+            }
+        });
+    }
+});
---
+++ app/view/table/TableInfoTab.js
@@ -0,0 +1,15 @@
+Ext.define('Planche.view.table.TableInfoTab', {
+    extend  : 'Ext.panel.Panel',
+    alias   : 'widget.table-info-tab',
+    title   : 'Table Information',
+    config : {
+        edited: false,
+        application : null,
+        database : null,
+        table : null
+    },
+    initComponent : function(){
+
+        this.callParent(arguments);
+    }
+});
