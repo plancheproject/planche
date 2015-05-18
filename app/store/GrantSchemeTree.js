deb47334d32943e61a7b0e94140a0781d410fb4f
jeongjuwon <jjwcom@nate.com>
2015-05-19 00:10:48 +0900

    Add root's path attribute

--- app/store/GrantSchemeTree.js
+++ app/store/GrantSchemeTree.js
@@ -2,6 +2,7 @@ Ext.define('Planche.store.GrantSchemeTree', {
     extend: 'Ext.data.TreeStore',
     root  : {
         type    : 'global',
+        path    : 'global',
         text    : 'Global Privileges',
         expanded: true
     }
