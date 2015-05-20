b10c9c1b28c9d30af8b111f3716ea3b566d539c0
jeongjuwon <jjwcom@nate.com>
2015-05-20 14:05:52 +0900

    Add connection  test feature Add ping method. Add connect by "keyenter" event

--- app/controller/connection/Connect.js
+++ app/controller/connection/Connect.js
@@ -1,95 +1,77 @@
 Ext.define('Planche.controller.connection.Connect', {
     extend: 'Ext.app.Controller',
 
-    initWindow : function () {
+    initWindow: function() {
 
-        var columns = this.makeListColumns();
+        var app = this.getApplication(),
+            columns = this.makeListColumns(),
+            fields = [];
 
-        var fields = [];
-        Ext.each(columns, function (obj) {
+        Ext.each(columns, function(obj) {
 
             fields.push(obj.dataIndex);
         });
 
         var store = Ext.create('Ext.data.Store', {
-            fields:fields
+            fields: fields
         });
 
+
         var grid = Ext.create('Ext.grid.Panel', {
-            id : 'connect-grid',
-            border : false,
+            id         : 'connect-grid',
+            border     : false,
             columnLines: true,
-            width : '100%',
-            flex  : 1,
-            columns : columns,
-            store: store,
-            listeners : {
-                scope : this,
-                itemdblclick : function (view, record) {
+            width      : '100%',
+            flex       : 1,
+            columns    : columns,
+            store      : store,
+            listeners  : {
+                scope      : this,
+                itemdblclick: this.connect,
+                cellkeydown: function(grid, td, cellIndex, record, tr, rowIndex, e, eOpts) {
 
-                    var win = view.up("window");
-                    var connInfo = record.raw;
+                    if(e.keyCode === Ext.EventObject.ENTER){
 
-                    var DBMS = connInfo.dbms || 'mysql';
-
-                    Ext.require('Planche.dbms.'+DBMS, function () {
-
-                        Ext.apply(connInfo, {
-                            DBMS : DBMS,
-                            APIS : Planche.dbms[DBMS]
-                        });
-
-                        win.hide();
-
-                        this.getApplication().initConnectTab(connInfo);
-                    }, this);
+                        this.connect();
+                    }
                 }
             }
         });
 
-        store.on('refresh', function (store, records, successful, eOpts) {
+        store.on('refresh', function(store, records, successful, eOpts) {
 
             this.getSelectionModel().select(store.getAt(0));
             //debugger;
         }, grid);
 
         Ext.create('Planche.lib.Window', {
-            id : 'window-'+this.id,
-            stateful: true,
-            title : 'Connect to MySQL Host',
-            layout : 'fit',
-            bodyStyle:"background-color:#FFFFFF",
-            width : 900,
-            height: 500,
-            overflowY: 'auto',
-            autoScroll : true,
-            modal : true,
-            plain: true,
-            fixed : true,
-            shadow : false,
-            autoShow : true,
+            id        : 'connect-window',
+            stateful  : true,
+            title     : 'Connect to MySQL Host',
+            layout    : 'fit',
+            bodyStyle : "background-color:#FFFFFF",
+            width     : 900,
+            height    : 500,
+            overflowY : 'auto',
+            autoScroll: true,
+            modal     : true,
+            plain     : true,
+            fixed     : true,
+            shadow    : false,
+            autoShow  : true,
             constrain : true,
-            items : grid,
-            buttons : [{
-                text : 'Connect',
-                scope : this,
-                handler : function (btn, e) {
-                    
-                    var win = btn.up('window');
-                    var sel = win.down('grid').getSelectionModel().getSelection();
-
-                    if(!sel) return;
-
-                    win.hide();
-
-                    this.getApplication().initConnectTab(sel[0].raw);
-                }
-            },{
-                text : 'Test Connect',
-                disabled: true
+            items     : grid,
+            buttons   : [{
+                text   : 'Connect',
+                scope  : this,
+                handler: this.connect
+            }, {
+                text   : 'Test Connect',
+                scope  : this,
+                handler: this.testConnect
             }],
             listeners : {
-                boxready : function () {
+                boxready: function() {
 
                     store.loadData(Planche.config.hosts);
                 }
@@ -97,28 +79,106 @@ Ext.define('Planche.controller.connection.Connect', {
         });
     },
 
-    makeListColumns : function () {   
-        
+    connect: function() {
+
+        var me = this,
+            app = me.getApplication();
+
+        this.ping(function() {
+
+            var win = me.getConnectWindow(),
+                conn = me.getSelectedConnection();
+
+            app.initConnectTab(conn.raw);
+
+            win.destroy();
+        });
+    },
+
+    testConnect: function() {
+
+        var me = this;
+
+        this.ping(function() {
+
+            var win = me.getConnectWindow();
+            win.setDisabled(false);
+
+            Ext.Msg.alert('Info', 'Connection is successful');
+        });
+    },
+
+    ping: function(callback) {
+
+        var win = this.getConnectWindow(),
+            app = this.getApplication(),
+            conn = this.getSelectedConnection();
+
+        win.setDisabled(true);
+
+        app.tunneling(Ext.apply({
+            query  : 'SELECT 1',
+            timeout: 5000,
+            success: function(config, response) {
+
+                callback();
+            },
+            failure: function(config, response) {
+
+                win.setDisabled(false);
+                Ext.Msg.alert('Error', response.message);
+            }
+        }, conn.raw));
+    },
+
+    getConnectWindow: function() {
+
+        return Ext.getCmp('connect-window');
+    },
+
+    getSelectedConnection: function() {
+
+        var grid = Ext.getCmp('connect-grid'),
+            selGrid = grid.selModel.getSelection();
+
+        if (selGrid.length == 0) {
+
+            return false;
+        }
+
+        return selGrid[0];
+    },
+
+    makeListColumns: function() {
+
         return [
-            { text: 'Host Name', dataIndex: 'hostName', width : 200, renderer: function (value, p, record) {
-                
+            {
+                text: 'Host Name', dataIndex: 'hostName', width: 200, renderer: function(value, p, record) {
+
                 return Ext.String.format('<img src=\'resources/images/icon_database24x24.png\'> {0}', value);
-            }},
-            { text: 'Host', dataIndex: 'host', width : 100, renderer: function (value, p, record) {
-                
+            }
+            },
+            {
+                text: 'Host', dataIndex: 'host', width: 100, renderer: function(value, p, record) {
+
                 return Ext.String.format('<img src=\'resources/images/icon_server24x24.png\'> {0}', value);
-            }},
-            { text: 'User', dataIndex: 'user', width : 100, renderer: function (value, p, record) {
-                
+            }
+            },
+            {
+                text: 'User', dataIndex: 'user', width: 100, renderer: function(value, p, record) {
+
                 return Ext.String.format('<img src=\'resources/images/icon_user24x24.png\'> {0}', value);
-            }},
-            { text: 'Request Type', dataIndex: 'requestType', width : 100, renderer : function(value){
+            }
+            },
+            {
+                text: 'Request Type', dataIndex: 'requestType', width: 100, renderer: function(value) {
 
                 return value ? value.toUpperCase() : 'AJAX';
-            }},
-            { text: 'Charset', dataIndex: 'charset', width : 100 },
-            { text: 'Port', dataIndex: 'port', width : 60 },
-            { text: 'HTTP Tunneling URL', dataIndex: 'tunnelingURL', flex : 1 }
+            }
+            },
+            {text: 'Charset', dataIndex: 'charset', width: 100},
+            {text: 'Port', dataIndex: 'port', width: 60},
+            {text: 'HTTP Tunneling URL', dataIndex: 'tunnelingURL', flex: 1}
         ];
     }
 });
