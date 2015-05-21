44679062c6dfabcea921bee404c4575b8b65b68f
jeongjuwon <jjwcom@nate.com>
2015-05-21 22:48:50 +0900

    [add] open column info, open indexes info, open root info

--- app/controller/layout/InfoTab.js
+++ app/controller/layout/InfoTab.js
@@ -1,221 +1,329 @@
 Ext.define('Planche.controller.layout.InfoTab', {
     extend: 'Ext.app.Controller',
-    init : function () {
+    init  : function() {
 
-    	this.control({
-    		'info-tab' : {
-		    	show : function (grid) {
+        this.control({
+            'info-tab'   : {
+                show: function(grid) {
 
-		    		this.openInfo(this.application.getSelectedNode());
-		    	}
-    		},
-			'scheme-tree' : {
-               select : function (view) {
+                    this.openInfo(this.application.getSelectedNode());
+                }
+            },
+            'scheme-tree': {
+                select: function(view) {
 
-                    var treeview = view.views[0];                       
-                    var tree = treeview.up("treepanel");
-
-                    var app = this.getApplication();
+                    var app = this.getApplication(),
+                        treeview = view.views[0],
+                        tree = treeview.up("treepanel");
 
                     app.setSelectedTree(tree);
 
                     var node = app.getSelectedNode();
 
-                    if(node.data.depth == 1) {
-
-                        var info = app.getActiveInfoTab();
-                        if(info.isVisible()) {
-
-                            this.openInfo(node);
-                        }
-                    }
-                    else if(node.data.depth == 2) {
-
-                        var info = app.getActiveInfoTab();
-                        if(info.isVisible()) {
-
-                            this.openInfo(node);
-                        }
-                    }
-                    else if(node.data.depth == 3) {
-
-                        var grid = app.getActiveTableDataTab();
-                        
-                        if(grid.isVisible() && (app.getParentNode(node, 2) == 'Tables' || app.getParentNode(node, 2) == 'Views')) {
-
-                            app.openTable(node)
-                        }
-
-                        var info = app.getActiveInfoTab();
-                        if(info.isVisible()) {
-
-                            this.openInfo(node);
-                        }
-                    }
+                    this.openInfo(node);
                 }
-			}
-    	});
+            }
+        });
     },
 
-	openInfo : function (node) {
+    openInfo : function(node){
 
-		var cmd = ['Database', 'Tables', 'Table'][node.data.depth - 1];
-		if(!cmd) { return; }
-        this[['open', cmd, 'Info'].join("")](node);
-	},
+        var app = this.getApplication(),
+            type = node.raw.type,
+            func = 'open' + type.charAt(0).toUpperCase() + type.slice(1) + 'Info';
 
-	openTableInfo : function (node) {
+        if(this[func]) {
 
-		var 
-		app			= this.getApplication(),
-		api			= app.getAPIS(),
-		db			= app.getParentNode(node),
-		info		= app.getActiveInfoTab(),
-		dom			= Ext.get(info.getEl().query("div[id$=innerCt]")),
-		queries		= {
-			create	: api.getQuery('TABLE_CREATE_INFO', db, node.data.text),
-			fields	: api.getQuery('SHOW_FULL_FIELDS', db, node.data.text),
-			keys	: api.getQuery('TABLE_KEYS_INFO', db, node.data.text)
-		},
-		keys		= Ext.Object.getKeys(queries),
-		responses	= {},
-		tunneling;
+            var info = app.getActiveInfoTab();
+            if (info.isVisible()) {
 
-		(tunneling = Ext.Function.bind(function () {
+                this[func](node);
+            }
+        }
+    },
 
-			var key = keys.shift();
-			if(key) {
+    openRootInfo: function(node) {
 
-				app.tunneling({
-					db : db,
-					query : queries[key],
-					node : node,
-					success : function (config, response) {
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            queries = {
+                databases: api.getQuery('SHOW_DATABASES')
+            },
+            keys = Ext.Object.getKeys(queries),
+            responses = {},
+            tunneling;
 
-						responses[key] = response;
-						tunneling();
-					}
-				});
-			}
-			else {
+        (tunneling = Ext.Function.bind(function() {
 
-		        var html = [];
-		        html.push('<h3>Show Table Fields</h3>');			        
-		        this.makeTableByRecord(responses.fields, html);
-		        html.push('<h3>Show Table Indexes</h3>');			        
-		        this.makeTableByRecord(responses.keys, html);
-		        html.push('<h3>Create Table DDL</h3>');
-		        html.push('<div class="info">'+responses.create.records[0][1].replace(/\n/gi, '<br/>')+'</div>');
-		        dom.setHTML(html.join(""));
-			}
+            var key = keys.shift();
+            if (key) {
 
-		}, this))();
-	},
+                app.tunneling({
+                    db     : db,
+                    query  : queries[key],
+                    node   : node,
+                    success: function(config, response) {
 
-	openTablesInfo : function (node) {
+                        responses[key] = response;
+                        tunneling();
+                    }
+                });
+            }
+            else {
 
-		var 
-		app		= this.getApplication(),
-		api		= app.getAPIS(),
-		db		= app.getParentNode(node),
-		info	= app.getActiveInfoTab(),
-		dom		= Ext.get(info.getEl().query("div[id$=innerCt]")),
-		me		= this;
+                var html = [];
+                html.push('<h3>Show Databases</h3>');
+                this.makeTableByRecord(responses.databases, html);
+                dom.setHTML(html.join(""));
+            }
 
-		app.tunneling({
-			db : db,
-			query : api.getQuery('SHOW_ALL_TABLE_STATUS', db),
-			node : node,
-			success : function (config, response) {
+        }, this))();
+    },
 
-		        var html = [];
-		        html.push('<h1>Show Table Status</h1>');
-		        me.makeTableByRecord(response, html);
-		        dom.setHTML(html.join(""));
-			}
-		});
-	},
+    openDatabaseInfo: function(node) {
 
-	openDatabaseInfo : function (node) {
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            queries = {
+                tables    : api.getQuery('SHOW_ALL_TABLE_STATUS', db),
+                views     : api.getQuery('SHOW_VIEWS', db),
+                procedures: api.getQuery('SHOW_PROCEDURES', db),
+                functions : api.getQuery('SHOW_FUNCTIONS', db),
+                triggers  : api.getQuery('SHOW_TRIGGERS', db),
+                events    : api.getQuery('SHOW_EVENTS', db),
+                ddl       : api.getQuery('SHOW_DATABASE_DDL', db)
+            },
+            keys = Ext.Object.getKeys(queries),
+            responses = {},
+            tunneling;
 
-		var 
-		app			= this.getApplication(),
-		api			= app.getAPIS(),
-		db			= app.getParentNode(node),
-		info		= app.getActiveInfoTab(),
-		dom			= Ext.get(info.getEl().query("div[id$=innerCt]")),
-		queries		= {
-			tables		: api.getQuery('SHOW_ALL_TABLE_STATUS', db),
-			views		: api.getQuery('SHOW_VIEWS', db),
-			procedures	: api.getQuery('SHOW_PROCEDURES', db),
-			functions	: api.getQuery('SHOW_FUNCTIONS', db),
-			triggers	: api.getQuery('SHOW_TRIGGERS', db),
-			events		: api.getQuery('SHOW_EVENTS', db),
-			ddl			: api.getQuery('SHOW_DATABASE_DDL', db)
-		},
-		keys		= Ext.Object.getKeys(queries),
-		responses	= {},
-		tunneling;
+        (tunneling = Ext.Function.bind(function() {
 
-		(tunneling = Ext.Function.bind(function () {
+            var key = keys.shift();
+            if (key) {
 
-			var key = keys.shift();
-			if(key) {
+                app.tunneling({
+                    db     : db,
+                    query  : queries[key],
+                    node   : node,
+                    success: function(config, response) {
 
-				app.tunneling({
-					db : db,
-					query : queries[key],
-					node : node,
-					success : function (config, response) {
+                        responses[key] = response;
+                        tunneling();
+                    }
+                });
+            }
+            else {
 
-						responses[key] = response;
-						tunneling();
-					}
-				});
-			}
-			else {
+                var html = [];
+                html.push('<h3>Table Information</h3>');
+                this.makeTableByRecord(responses.tables, html);
+                html.push('<h3>View Information</h3>');
+                this.makeTableByRecord(responses.views, html);
+                html.push('<h3>Procedure Information</h3>');
+                this.makeTableByRecord(responses.procedures, html);
+                html.push('<h3>Function Information</h3>');
+                this.makeTableByRecord(responses.functions, html);
+                html.push('<h3>Trigger Information</h3>');
+                this.makeTableByRecord(responses.triggers, html);
+                html.push('<h3>Event Information</h3>');
+                this.makeTableByRecord(responses.events, html);
+                html.push('<h3>Create Database DDL</h3>');
+                html.push('<div class="info">' + responses.ddl.records[0][1].replace(/\n/gi, '<br/>') + '</div>');
+                dom.setHTML(html.join(""));
+            }
 
-		        var html = [];
-		        html.push('<h3>Table Information</h3>');        
-		        this.makeTableByRecord(responses.tables, html);
-		        html.push('<h3>View Information</h3>');
-		        this.makeTableByRecord(responses.views, html);
-		        html.push('<h3>Procedure Information</h3>');        
-		        this.makeTableByRecord(responses.procedures, html);
-		        html.push('<h3>Function Information</h3>');        
-		        this.makeTableByRecord(responses.functions, html);
-		        html.push('<h3>Trigger Information</h3>');        
-		        this.makeTableByRecord(responses.triggers, html);
-		        html.push('<h3>Event Information</h3>');        
-		        this.makeTableByRecord(responses.events, html);
-		        html.push('<h3>Create Database DDL</h3>');
-		        html.push('<div class="info">'+responses.ddl.records[0][1].replace(/\n/gi, '<br/>')+'</div>');
-		        dom.setHTML(html.join(""));
-			}
+        }, this))();
+    },
 
-		}, this))();
-	},
+    openTablesInfo: function(node) {
 
-	makeTableByRecord : function (record, html) {
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            me = this;
 
-		var html = html || [];
+        app.tunneling({
+            db     : db,
+            query  : api.getQuery('SHOW_ALL_TABLE_STATUS', db),
+            node   : node,
+            success: function(config, response) {
+
+                var html = [];
+                html.push('<h3>Show Table Status</h3>');
+                me.makeTableByRecord(response, html);
+                dom.setHTML(html.join(""));
+            }
+        });
+    },
+
+    openTableInfo: function(node) {
+
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            queries = {
+                create: api.getQuery('TABLE_CREATE_INFO', db, node.data.text),
+                fields: api.getQuery('SHOW_FULL_FIELDS', db, node.data.text),
+                keys  : api.getQuery('TABLE_KEYS_INFO', db, node.data.text)
+            },
+            keys = Ext.Object.getKeys(queries),
+            responses = {},
+            tunneling;
+
+        (tunneling = Ext.Function.bind(function() {
+
+            var key = keys.shift();
+            if (key) {
+
+                app.tunneling({
+                    db     : db,
+                    query  : queries[key],
+                    node   : node,
+                    success: function(config, response) {
+
+                        responses[key] = response;
+                        tunneling();
+                    }
+                });
+            }
+            else {
+
+                var html = [];
+                html.push('<h3>Show Table Fields</h3>');
+                this.makeTableByRecord(responses.fields, html);
+                html.push('<h3>Show Table Indexes</h3>');
+                this.makeTableByRecord(responses.keys, html);
+                html.push('<h3>Create Table DDL</h3>');
+                html.push('<div class="info">' + responses.create.records[0][1].replace(/\n/gi, '<br/>') + '</div>');
+                dom.setHTML(html.join(""));
+            }
+
+        }, this))();
+    },
+
+    openColumnsInfo: function(node) {
+
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            queries = {
+                fields: api.getQuery('SHOW_FULL_FIELDS', db, node.data.text)
+            },
+            keys = Ext.Object.getKeys(queries),
+            responses = {},
+            tunneling;
+
+        (tunneling = Ext.Function.bind(function() {
+
+            var key = keys.shift();
+            if (key) {
+
+                app.tunneling({
+                    db     : db,
+                    query  : queries[key],
+                    node   : node,
+                    success: function(config, response) {
+
+                        responses[key] = response;
+                        tunneling();
+                    }
+                });
+            }
+            else {
+
+                var html = [];
+                html.push('<h3>Show Table Fields</h3>');
+                this.makeTableByRecord(responses.fields, html);
+                dom.setHTML(html.join(""));
+            }
+
+        }, this))();
+    },
+
+    openIndexesInfo: function(node) {
+
+        var app = this.getApplication(),
+            api = app.getAPIS(),
+            db = app.getParentNode(node),
+            info = app.getActiveInfoTab(),
+            dom = Ext.get(info.getEl().query("div[id$=innerCt]")),
+            queries = {
+                keys  : api.getQuery('TABLE_KEYS_INFO', db, node.data.text)
+            },
+            keys = Ext.Object.getKeys(queries),
+            responses = {},
+            tunneling;
+
+        (tunneling = Ext.Function.bind(function() {
+
+            var key = keys.shift();
+            if (key) {
+
+                app.tunneling({
+                    db     : db,
+                    query  : queries[key],
+                    node   : node,
+                    success: function(config, response) {
+
+                        responses[key] = response;
+                        tunneling();
+                    }
+                });
+            }
+            else {
+
+                var html = [];
+                html.push('<h3>Show Table Indexes</h3>');
+                this.makeTableByRecord(responses.keys, html);
+                dom.setHTML(html.join(""));
+            }
+
+        }, this))();
+    },
+
+    makeTableByRecord: function(record, html) {
+
+        var html = html || [];
         html.push('<table class="info" width="100%">');
         html.push('<tr>');
-        Ext.Array.each(record.fields, function (col, cidx) {
+        Ext.Array.each(record.fields, function(col, cidx) {
 
-            html.push('<th>'+col.name+'</th>');
+            html.push('<th>' + col.name + '</th>');
         });
         html.push('</tr>');
-        Ext.Array.each(record.records, function (row, ridx) {
+
+        if(record.records.length === 0){
 
             html.push('<tr>');
-            Ext.Array.each(record.fields, function (col, cidx) {
+            html.push('<td colspan="'+record.fields.length+'">There\'s no data to display</td>');
+            html.push('</tr>');
+            html.push('</table>');
+            return html;
+        }
 
-                html.push('<td>'+row[cidx]+'</td>');
+        Ext.Array.each(record.records, function(row, ridx) {
+
+            html.push('<tr>');
+            Ext.Array.each(record.fields, function(col, cidx) {
+
+                html.push('<td>' + row[cidx] + '</td>');
             });
             html.push('</tr>');
         });
         html.push('</table>');
         return html;
-	}
+    }
 });
