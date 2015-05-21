1c49ff5e2511070d932714c8e7c2087d984a0d87
jeongjuwon <jjwcom@nate.com>
2015-05-21 13:01:07 +0900

    [add] display tunneling messages..

--- resources/tunneling/php/planche_tnl.php
+++ resources/tunneling/php/planche_tnl.php
@@ -1,52 +1,52 @@
-<?php 
+<?php
 define('DEBUG', true);
 
-if(DEBUG == true) {
+if (DEBUG == true) {
 
     error_reporting(E_ALL);
-}
-else {
+} else {
 
     error_reporting(0);
 }
+
+$sAddr = "localhost";
+$nPort = 8888;
 
 set_time_limit(0);
 
 $isCLI = php_sapi_name() == "cli";
 
-if (!function_exists('debug'))
-{
-    function debug($msg){
+if (!function_exists('debug')) {
+    function debug($msg)
+    {
 
-        $fp = fopen ("tunnel.log", "a");
-        if($fp == false){
+        $fp = fopen("tunnel.log", "a");
+        if ($fp == false) {
 
             return;
         }
-        fwrite($fp, "[".date("Y-m-d H:i:s")."]".$msg." \r\n" );
+        fwrite($fp, "[".date("Y-m-d H:i:s")."]".$msg." \r\n");
         fclose($fp);
     }
 }
 
-if (!function_exists('http_parse_headers'))
-{
+if (!function_exists('http_parse_headers')) {
     function http_parse_headers($raw_headers)
     {
         $headers = array();
-        $key = ''; // [+]
+        $key     = ''; // [+]
 
-        foreach(explode("\n", $raw_headers) as $i => $h)
-        {   
+        foreach (explode("\n", $raw_headers) as $i => $h) {
             $pos = strpos($h, "GET");
 
-            if($pos > -1){
+            if ($pos > -1) {
 
-                $get = trim(substr($h, $pos + 3)); 
+                $get = trim(substr($h, $pos + 3));
                 $get = explode(" ", $get);
 
                 $pos = strpos($get[0], "?");
 
-                if($pos === false){
+                if ($pos === false) {
 
                     $headers['URL'] = $get[0];
                     continue;
@@ -59,61 +59,65 @@ if (!function_exists('http_parse_headers'))
 
             $pos = strpos($h, ":");
 
-            $headers[substr($h, 0, $pos)] = trim(substr($h, $pos+2));
+            $headers[substr($h, 0, $pos)] = trim(substr($h, $pos + 2));
         }
 
         return $headers;
     }
 }
 
-if (!function_exists('writeResponse'))
-{
+if (!function_exists('writeResponse')) {
     function writeResponse($string, $client = null)
     {
-        if($client){
+        if ($client) {
 
             socket_write($client, $string, strlen($string));
-        }
-        else {
+        } else {
 
             echo $string;
         }
     }
 }
 
-class Control {
+class Control
+{
 
-    public $conn;
-    private $types = array();
+    public  $conn;
+    private $types    = array();
     private $callback = null;
-    private $client = null;
+    private $client   = null;
 
-    public function __construct(){
+    public function __construct()
+    {
 
         $this->loadDataTypes();
     }
 
-    public function connect($host, $user, $pass, $db){
+    public function connect($host, $user, $pass, $db)
+    {
 
         $this->conn = @new mysqli($host, $user, $pass, $db);
 
         if ($this->conn->connect_error) {
-            
-            $this->error('Connect Error (' . $this->conn->connect_errno . ') '. $this->conn->connect_error);
+
+            $this->error('Connect Error ('.$this->conn->connect_errno.') '.$this->conn->connect_error);
+
             return false;
         }
 
         return true;
     }
 
-    public function setClient($client){
+    public function setClient($client)
+    {
 
         $this->client = $client;
     }
 
-    public function sendHeader(){
+    public function sendHeader()
+    {
 
-        if($this->client){
+        if ($this->client) {
 
             writeResponse("HTTP/1.1 200 OK \r\n", $this->client);
             writeResponse("Date: Fri, 31 Dec 1999 23:59:59 GMT \r\n", $this->client);
@@ -121,18 +125,17 @@ class Control {
             if ($this->callback) {
 
                 writeResponse("Content-Type: application/javascript \r\n\r\n", $this->client);
-                
+
             } else {
 
                 writeResponse("Content-Type: application/json \r\n\r\n", $this->client);
             }
-        }
-        else {
+        } else {
 
             if ($this->callback) {
 
                 header('Content-Type: application/javascript');
-                
+
             } else {
 
                 header('Content-Type: application/json');
@@ -140,60 +143,65 @@ class Control {
         }
     }
 
-    public function setCharset($charset){
+    public function setCharset($charset)
+    {
 
-        if (empty($charset) === true){
+        if (empty($charset) === true) {
 
             return;
         }
 
-        $query = 'SET NAMES ' . $charset;
+        $query = 'SET NAMES '.$charset;
         $this->conn->query($query);
     }
 
-    public function error($error){
+    public function error($error)
+    {
 
-        $this->output(array (
+        $this->output(array(
             'success' => false,
             'message' => $error
         ));
     }
 
-    public function setCallback($callback){
+    public function setCallback($callback)
+    {
 
         $this->callback = $callback;
     }
 
-    public function loadDataTypes(){
+    public function loadDataTypes()
+    {
 
         $constants = get_defined_constants(true);
-        foreach($constants['mysqli'] as $idx => $val){
+        foreach ($constants['mysqli'] as $idx => $val) {
 
             $prefix = substr($idx, 0, 11);
-            if($prefix == 'MYSQLI_TYPE'){
+            if ($prefix == 'MYSQLI_TYPE') {
 
                 $this->types[$val] = strtolower(substr($idx, 12));
             }
         }
     }
 
-    public function query($query = null){
-
-
-        if(!$query){
+    public function query($query = null)
+    {
+        if (!$query) {
 
             $this->error('query was empty');
+
             return;
         }
 
-        $start = substr(microtime(), 0, 10);
-        $result = $this->conn->query($query);
-        $end = substr(microtime(), 0, 10);
+        $start     = substr(microtime(), 0, 10);
+        $result    = $this->conn->query($query);
+        $end       = substr(microtime(), 0, 10);
         $exec_time = substr($end - $start, 0, 10);
 
         if ($this->conn->error) {
 
             $this->error($this->conn->error);
+
             return;
         }
 
@@ -216,31 +224,29 @@ class Control {
         $insert_id = $this->conn->insert_id;
         writeResponse('"insert_id":'.$insert_id.',', $this->client);
 
-        if(method_exists($result, 'fetch_fields')){
+        if (method_exists($result, 'fetch_fields')) {
 
             $fields = $result->fetch_fields();
-            foreach($fields as $idx => $row){
+            foreach ($fields as $idx => $row) {
 
-                if(preg_match("/([^a-zA-Z0-9_$#])+/", $row->name, $matches)){
+                if (preg_match("/([^a-zA-Z0-9_$#])+/", $row->name, $matches)) {
 
                     $name = "tmp".$idx;
-                }
-                else {
+                } else {
 
                     $name = $row->name;
                 }
 
                 array_push($this->fields, array(
-                    'name' => $name,
-                    'type' => $this->types[$row->type],
-                    'table' => $row->table,
+                    'name'       => $name,
+                    'type'       => $this->types[$row->type],
+                    'table'      => $row->table,
                     'max_length' => $row->max_length
                 ));
             }
 
             $is_result_query = true;
-        }
-        else {
+        } else {
 
             $is_result_query = false;
         }
@@ -249,15 +255,14 @@ class Control {
 
         writeResponse('"records":[', $this->client);
 
-        if(method_exists($result, 'fetch_array')){
+        if (method_exists($result, 'fetch_array')) {
 
             $idx = 0;
-            while ($row = $result->fetch_array(MYSQLI_ASSOC))
-            {
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
                 $this->field_idx = 0;
 
-                if($idx > 0){ 
-                    
+                if ($idx > 0) {
+
                     writeResponse(",", $this->client);
                 }
 
@@ -265,34 +270,37 @@ class Control {
                 $idx++;
             }
         }
-        
+
         writeResponse("],", $this->client);
 
-        writeResponse('"is_result_query":'.($is_result_query ? 'true':'false').",", $this->client);
+        writeResponse('"is_result_query":'.($is_result_query ? 'true' : 'false').",", $this->client);
 
-        $end = substr(microtime(), 0, 10);
+        $end           = substr(microtime(), 0, 10);
         $transfer_time = substr($end - $start, 0, 10);
 
         writeResponse('"transfer_time":'.$transfer_time.',', $this->client);
 
         $total_time = $exec_time + $transfer_time;
-        
+
         writeResponse('"total_time":'.$total_time, $this->client);
         writeResponse("}", $this->client);
 
-        if($this->callback !== null){
+        if ($this->callback !== null) {
 
             writeResponse(');', $this->client);
         }
     }
 
-    public function removeHTML($value){
+    public function removeHTML($value)
+    {
 
         $this->field_idx++;
+
         return $value;
     }
 
-    public function output($output){
+    public function output($output)
+    {
 
         $this->sendHeader();
 
@@ -310,7 +318,8 @@ class Control {
 
     }
 
-    public function export(){
+    public function export()
+    {
 
     }
 }
@@ -319,33 +328,42 @@ $Planche = new Control();
 
 if ($isCLI) {
 
-    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
-    if(!$sock){
+    echo "-----------------------------------------------------------------------\n";
+    echo "Start Planche Tunneling Server\n";
 
-        echo 'Create socket failed.';
+    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
+    if (!$sock) {
+
+        echo "ERROR : Create socket failed.\n";
         exit;
     }
 
-    socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);   
-    $bResult = socket_bind($sock, 'localhost', 8888);
+    socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
+    $bResult = socket_bind($sock, $sAddr, $nPort);
 
-    if(!$bResult){
+    if (!$bResult) {
 
-       echo 'Socket address was not bound. '."$sAddr:$nPort";
+        echo "ERROR : Socket address was not bound. $sAddr:$nPort\n";
+        exit;
     }
 
-    socket_listen($sock);  
+    echo "Mapping http://$sAddr:$nPort/ to ....\n";
+    echo "-----------------------------------------------------------------------\n";
+    echo "HTTP tunneling server is ready at : http://$sAddr:$nPort\n";
+    echo "-----------------------------------------------------------------------\n";
 
-    while(true)
-    {
+    socket_listen($sock);
+
+    while (true) {
+
         $client = socket_accept($sock);
 
-        if ($client === false) 
-        { 
+        if ($client === false) {
+
             usleep(100);
-        }
-        else if ($client > 0)  {
-            
+
+        } else if ($client > 0) {
+
             $Planche->setClient($client);
 
             socket_getpeername($client, $sClientIp, $nClientPort);
@@ -354,13 +372,13 @@ if ($isCLI) {
 
             $aHeaders = http_parse_headers($sRequestHeader);
 
-            if($aHeaders['URL'] == '/favicon.ico'){
+            if ($aHeaders['URL'] == '/favicon.ico') {
 
                 socket_close($client);
                 continue;
             }
 
-            if(isset($aHeaders['GET']) === false){
+            if (isset($aHeaders['GET']) === false) {
 
                 $Planche->error('Invalid request');
                 socket_close($client);
@@ -369,11 +387,32 @@ if ($isCLI) {
 
             extract($aHeaders['GET']);
 
-            if($callback) $Planche->setCallback($callback);
-            if($Planche->connect($host, $user, $pass, $db)){
+            echo "Execute Query\n";
+
+            if ($callback) {
+
+                echo "JSON Callback : $callback\n";
+                $Planche->setCallback($callback);
+            }
+
+            echo "Connect $host, $user, **************, $db\n";
+            if ($Planche->connect($host, $user, $pass, $db)) {
+
+                echo "Connection Successful\n";
+
+                echo "The execution SQL is\n";
+
+                echo $query."\n";
 
                 $Planche->setCharset($charset);
-                $Planche->query($query);                
+                $Planche->query($query);
+
+                echo "-----------------------------------------------------------------------\n";
+            }
+            else {
+
+                echo "ERROR : CONNECTION FAILED $host, $user, $pass, $db\n";
+                echo "-----------------------------------------------------------------------\n";
             }
 
             socket_close($client);
@@ -384,7 +423,7 @@ if ($isCLI) {
 
     extract($_GET);
 
-    if($callback) $Planche->setCallback($callback);
+    if ($callback) $Planche->setCallback($callback);
 
     $Planche->connect($host, $user, $pass, $db);
     $Planche->setCharset($charset);
