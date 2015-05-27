b1c968a694727412f9c2913d782a807450192898
jeongjuwon <jjwcom@nate.com>
2015-05-27 11:46:18 +0900

    build

--- build/production/Planche/resources/tunneling/nodejs/planche_tnl.js
+++ build/production/Planche/resources/tunneling/nodejs/planche_tnl.js
@@ -1,9 +1,9 @@
-var mysql = require('mysql'),
-	http  = require('http'),
-	url   = require('url'),
-	querystring = require('querystring');
+var mysql       = require('mysql'),
+    http        = require('http'),
+    url         = require('url'),
+    querystring = require('querystring');
 
-function Control() { 
+function Control() {
 
     this.conn = null;
     this.types = {};
@@ -15,132 +15,139 @@ function Control() {
 
 Control.prototype = {
 
-	connect : function (host, user, pass, db, response){
+    connect: function(host, user, pass, db, response) {
 
-		this.response = response;
+        this.response = response;
 
-		this.conn = mysql.createConnection({
-			host     : host,
-			user     : user,
-			password : pass,
-			database : db
-		});
+        this.conn = mysql.createConnection({
+            host    : host,
+            user    : user,
+            password: pass,
+            database: db
+        });
 
-		var me = this;
-		this.conn.connect(function(err) {
+        var me = this;
 
-			if(!err){
+        this.conn.connect(function(err) {
 
-				return;
-			}
-			
-		    me.error('Connect Error ('+err.errno+') '+err.code);
-		});
-	},
+            if (!err) {
 
-    sendHeader : function(){
+                console.log("Connect " + host + ":" + user + ", **************, " + db);
+                console.log("Connection Successful");
+                console.log("-----------------------------------------------------------------------");
+                return;
+            }
+
+            me.error('Connect Error (' + err.errno + ') ' + err.code);
+
+            console.log("ERROR : CONNECTION FAILED " + host + ":" + user + ", **************, " + db);
+            console.log("-----------------------------------------------------------------------");
+        });
+    },
+
+    sendHeader: function() {
 
         if (this.callback) {
 
-            this.response.writeHead(200, {'Content-Type' : 'application/javascript'});
-            
+            this.response.writeHead(200, {'Content-Type': 'application/javascript'});
+
         } else {
 
-            this.response.writeHead(200, {'Content-Type' : 'application/json'});
+            this.response.writeHead(200, {'Content-Type': 'application/json'});
         }
     },
 
-    setCharset : function(charset){
+    setCharset: function(charset) {
 
-        if (!charset){
+        if (!charset) {
 
             return;
         }
 
-        var query = 'SET NAMES '+charset;
+        var query = 'SET NAMES ' + charset;
 
-		this.conn.query(query);
+        this.conn.query(query);
     },
 
-    setCallback : function(callback){
+    setCallback: function(callback) {
 
         this.callback = callback;
     },
 
-    loadDataTypes : function(){
+    loadDataTypes: function() {
 
-		var types = ['TINYINT', 'SMALLINT', 'INT', 'MEDIUMINT', 'YEAR', 'FLOAT', 'DOUBLE', 'TIMESTAMP', 'DATE', 'DATETIME', 'TINYBLOB', 'MEDIUMBLOB', 'LONGBLOB', 'BLOB', 'BINARY', 'VARBINARY', 'BIT', 'CHAR', 'VARCHAR', 'TINYTEXT', 'MEDIUMTEXT', 'LONGTEXT', 'TEXT', 'ENUM', 'SET', 'DECIMAL', 'BIGINT', 'TIME', 'GEOMETRY'];
-		var me = this;
-		types.forEach(function(type, idx){
+        var types = ['TINYINT', 'SMALLINT', 'INT', 'MEDIUMINT', 'YEAR', 'FLOAT', 'DOUBLE', 'TIMESTAMP', 'DATE', 'DATETIME', 'TINYBLOB', 'MEDIUMBLOB', 'LONGBLOB', 'BLOB', 'BINARY', 'VARBINARY', 'BIT', 'CHAR', 'VARCHAR', 'TINYTEXT', 'MEDIUMTEXT', 'LONGTEXT', 'TEXT', 'ENUM', 'SET', 'DECIMAL', 'BIGINT', 'TIME', 'GEOMETRY'];
+        var me = this;
+        types.forEach(function(type, idx) {
 
-			me.types[type] = type.toLowerCase();
-		});
-	},
+            me.types[type] = type.toLowerCase();
+        });
+    },
 
-    query : function(query){
+    query: function(query) {
 
-        if(!query){
+        if (!query) {
 
             this.error('query was empty');
             return;
         }
 
         var start = new Date().getTime(),
-        	affected_rows = 0,
-        	insert_id = 0,
-        	fields = [];
-        	
+            affected_rows = 0,
+            insert_id = 0,
+            fields = [];
+
         var me = this,
-        	result = this.conn.query(query, function(err, results, fields){
+            result = this.conn.query(query, function(err, results, fields) {
 
-        		if(err){
+                if (err) {
 
-        			me.failure(err);
-        			return;
-        		}
+                    me.failure(err);
+                    return;
+                }
 
-				var end = new Date().getTime();
- 
-        		me.success(fields, results, start, end);	
-        	});
+                var end = new Date().getTime();
+
+                me.success(fields, results, start, end);
+            });
     },
 
-    success : function(fetchFields, fetchRows, start, end){
+    success: function(fetchFields, fetchRows, start, end) {
 
-    	var insert_id = fetchRows.insertId || 0,
-    		affected_rows = 0,
-    		is_result_query = false,
-    		fields = [],
-    		me = this;
+        var insert_id = fetchRows.insertId || 0,
+            affected_rows = 0,
+            is_result_query = false,
+            fields = [],
+            me = this;
 
-    	if(fetchRows.affectedRows){
+        if (fetchRows.affectedRows) {
 
-    		affected_rows = fetchRows.affectedRows;
-    	}
+            affected_rows = fetchRows.affectedRows;
+        }
 
-    	if(fetchRows.changedRows){
+        if (fetchRows.changedRows) {
 
-    		affected_rows = fetchRows.changedRows;
-    	}
+            affected_rows = fetchRows.changedRows;
+        }
 
         this.sendHeader();
 
         if (this.callback !== null) {
 
-            this.response.write(this.callback+'(');
+            this.response.write(this.callback + '(');
         }
-	    
-		exec_time = end - start;
 
-		if(fetchFields){
+        exec_time = end - start;
 
-			is_result_query = true;
+        if (fetchFields) {
 
-            fetchFields.forEach(function(field, idx){
+            is_result_query = true;
 
-                if(field.name.match(/([^a-zA-Z0-9_$#])+/)){
+            fetchFields.forEach(function(field, idx) {
 
-                    var name = "tmp"+idx;
+                if (field.name.match(/([^a-zA-Z0-9_$#])+/)) {
+
+                    var name = "tmp" + idx;
                 }
                 else {
 
@@ -148,34 +155,34 @@ Control.prototype = {
                 }
 
                 fields.push({
-                    'name' : name,
-                    'type' : me.types[field.type],
-                    'table' : field.table,
-                    'max_length' : field.length
+                    'name'      : name,
+                    'type'      : me.types[field.type],
+                    'table'     : field.table,
+                    'max_length': field.length
                 });
             });
-		}
+        }
 
         this.response.write('{"success":true,');
-        this.response.write('"exec_time":'+exec_time+',');
-        this.response.write('"affected_rows":'+affected_rows+',');
-        this.response.write('"insert_id":'+insert_id+',');
-        this.response.write('"fields":'+JSON.stringify(fields)+',');
+        this.response.write('"exec_time":' + exec_time + ',');
+        this.response.write('"affected_rows":' + affected_rows + ',');
+        this.response.write('"insert_id":' + insert_id + ',');
+        this.response.write('"fields":' + JSON.stringify(fields) + ',');
         this.response.write('"records":[');
 
         idx = 0;
 
-        fetchRows.forEach(function(row){
+        fetchRows.forEach(function(row) {
 
-            if(idx > 0){ 
-                
+            if (idx > 0) {
+
                 me.response.write(",");
             }
 
             var tmp = [];
-            for(var p in row){
+            for (var p in row) {
 
-            	tmp.push(row[p]);
+                tmp.push(row[p]);
             }
 
             me.response.write(JSON.stringify(tmp));
@@ -184,19 +191,19 @@ Control.prototype = {
 
         this.response.write("],");
 
-        this.response.write('"is_result_query":'+(is_result_query ? 'true':'false')+",");
+        this.response.write('"is_result_query":' + (is_result_query ? 'true' : 'false') + ",");
 
         var end = new Date().getTime(),
-        	transfer_time = end - start;
+            transfer_time = end - start;
 
-        this.response.write('"transfer_time":'+transfer_time+',');
+        this.response.write('"transfer_time":' + transfer_time + ',');
 
         total_time = exec_time + transfer_time;
-        
-        this.response.write('"total_time":'+total_time);
+
+        this.response.write('"total_time":' + total_time);
         this.response.write("}");
 
-        if(this.callback !== null){
+        if (this.callback !== null) {
 
             this.response.write(');');
         }
@@ -204,29 +211,29 @@ Control.prototype = {
         this.response.end('\n');
     },
 
-    failure : function(err){
+    failure: function(err) {
 
-    	this.conn.end();
-    	this.error(err.errno+' : '+err.code);
+        this.conn.end();
+        this.error(err.errno + ' : ' + err.code);
     },
 
-    error : function(error){
+    error: function(error) {
 
         this.output({
-            'success' : false,
-            'message' : error
+            'success': false,
+            'message': error
         });
     },
 
-    output : function(output){
+    output: function(output) {
 
         this.sendHeader();
 
         if (this.callback) {
 
-            this.response.write(this.callback+'(');
+            this.response.write(this.callback + '(');
         }
-		
+
         this.response.write(JSON.stringify(output));
 
         if (this.callback) {
@@ -239,28 +246,48 @@ Control.prototype = {
     }
 }
 
-var DEBUG = true;
-var Planche = new Control();
+var DEBUG   = true,
+    Planche = new Control(),
+    port    = 8888,
+    addr    = 'localhost';
+
+console.log("-----------------------------------------------------------------------");
+console.log("Start Planche Tunneling Server");
+console.log("Mapping http://" + addr + ":" + port + "/ to ....");
+console.log("-----------------------------------------------------------------------");
+console.log("HTTP tunneling server is ready at : http://" + addr + ":" + port);
+console.log("-----------------------------------------------------------------------");
 
 //Create Webserver
 http.createServer(function(request, response) {
 
-	if (request.url == '/favicon.ico') {
+    if (request.url == '/favicon.ico') {
 
-		response.end('\n');
-		return;
-	}
+        response.end('\n');
+        return;
+    }
 
-	var parse = url.parse(request.url),
-		get = querystring.parse(parse.query);
+    var parse = url.parse(request.url),
+        get = querystring.parse(parse.query);
 
-	if(get.callback){
+    console.log("Execute Query");
 
-		Planche.setCallback(get.callback);
-	}
+    if (get.callback) {
 
-	Planche.connect(get.host, get.user, get.pass, get.db, response);
-	Planche.setCharset(get.charset);
-	Planche.query(get.query);
+        console.log("JSON Callback : " + get.callback);
+        Planche.setCallback(get.callback);
+    }
 
-}).listen(8888);
+    Planche.connect(get.host, get.user, get.pass, get.db, response);
+
+    console.log("The execution SQL is");
+
+    console.log(get.query);
+
+
+    Planche.setCharset(get.charset);
+    Planche.query(get.query);
+
+    console.log("-----------------------------------------------------------------------");
+
+}).listen(port, addr);
