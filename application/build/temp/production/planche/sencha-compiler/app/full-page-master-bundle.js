1b3596deaa23a71544125c7c5533a898b53830e0
jeongjuwon <jjwcom@nate.com>
2016-11-28 19:06:05 +0900

    Remove legacy structure

--- application/build/temp/production/planche/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/hanalabs/Documents/workspace/planche-project/planche/application/app.js
--- build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf build
-cd application
-sencha app build
-cp -rf  build/production/planche ../build
